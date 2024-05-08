c45969ad0131b569e9ade19f0ea43ef15e0af0b6
MARCO KRIESSMANN <kriessmann-marco@s-giken.com>
2024-05-08 11:40:19 +0900

    Implemented Crud in DB Handler

--- DatabaseHandler/DatabaseHandler.vb
+++ DatabaseHandler/DatabaseHandler.vb
@@ -3,20 +3,34 @@ Imports Npgsql
 
 
 Namespace DatabaseHandling
-    Module DatabaseHandler
-        Sub TestConnection()
-            Dim connString As String = "Host=172.27.200.231;Port=5432;Database=kmarcodb;Username=kmarco;Password=kmarco"
+    'TODO:Switch to SQL lite
+    Public Class DatabaseHandler
+
+        Dim connString As String = "Host=172.27.200.231;Port=15432;Database=kmarco;Username=kmarco;Password=kmarco"
+
+
+        Public Sub TestConnection()
+            Dim flag As Boolean
+
             Using conn As New NpgsqlConnection(connString)
                 Try
                     conn.Open()
                     Console.WriteLine("Connected to PostgreSQL database!")
+                    flag = True
                 Catch ex As Exception
                     Console.WriteLine("Failed to connect to PostgreSQL database: " & ex.Message)
+                    flag = False
                 End Try
+
+                Console.WriteLine(flag)
+
+
             End Using
         End Sub
-        Function GetBlogPosts() As List(Of BlogPost)
-            Dim connString As String = "Host=172.27.200.231;Port=5432;Database=kmarcodb;Username=kmarco;Password=kmarco"
+
+
+
+        Public Function GetBlogPosts() As List(Of BlogPost)
 
             Dim posts As New List(Of BlogPost)()
             Using conn As New NpgsqlConnection(connString)
@@ -24,12 +38,12 @@ Namespace DatabaseHandling
 
                     Try
                         conn.Open()
-                        Console.WriteLine("Connected to PostgreSQL database!")
+                        'Console.WriteLine("Connected to PostgreSQL database!")
                     Catch ex As Exception
-                        Console.WriteLine("Failed to connect to PostgreSQL database: " & ex.Message)
+                        'Console.WriteLine("Failed to connect to PostgreSQL database: " & ex.Message)
                     End Try
 
-                    'TODO: get all blog postts and return list of blog posts
+                    'TODO: get all blog posts and return list of blog posts
                     Using reader As NpgsqlDataReader = command.ExecuteReader()
                         While reader.Read()
                             Dim post As New BlogPost()
@@ -48,6 +62,62 @@ Namespace DatabaseHandling
             Return posts
         End Function
 
+        Public Sub UpdatePost(ByVal blogpost As BlogPost)
 
-    End Module
+            Using conn As New NpgsqlConnection(connString)
+                Dim sqlCommand As String = "UPDATE BlogPosts SET title =" + blogpost.Title + ", content=" + blogpost.Content + "WHERE id =" + blogpost.Id + ";"
+                Using command As New NpgsqlCommand(sqlCommand, conn)
+
+                    Try
+                        conn.Open()
+                        'Console.WriteLine("Connected to PostgreSQL database!")
+                    Catch ex As Exception
+                        'Console.WriteLine("Failed to connect to PostgreSQL database: " & ex.Message)
+                    End Try
+                    command.ExecuteNonQuery()
+
+                    'TODO: get all blog posts and return list of blog posts
+                End Using
+
+            End Using
+
+        End Sub
+
+        Public Sub AddPost(ByVal blogpost As BlogPost)
+
+            Using conn As New NpgsqlConnection(connString)
+                'error prone, replace with better string concatenation
+                Dim sqlCommand As String = "INSERT INTO BlogPosts (id, title, content, date)  VALUES (" + blogpost.Id + ", '" + blogpost.Title + "', '" + blogpost.Content + "'," + Convert.ToString(blogpost.PostDate) + ";"
+                Using command As New NpgsqlCommand(sqlCommand, conn)
+
+                    Try
+                        conn.Open()
+                        'Console.WriteLine("Connected to PostgreSQL database!")
+                    Catch ex As Exception
+                        'Console.WriteLine("Failed to connect to PostgreSQL database: " & ex.Message)
+                    End Try
+                    command.ExecuteNonQuery()
+
+                    'TODO: get all blog posts and return list of blog posts
+                End Using
+
+            End Using
+
+        End Sub
+
+        Public Sub DeletePost(ByVal postId As Integer)
+            Dim sqlCommand As String = "DELETE FROM BlogPosts WHERE id = @id"
+
+            Using conn As New NpgsqlConnection(connString)
+                Using command As New NpgsqlCommand(SqlCommand, conn)
+                    command.Parameters.AddWithValue("@PostID", postId)
+
+                    conn.Open()
+                    command.ExecuteNonQuery()
+                End Using
+            End Using
+        End Sub
+
+
+    End Class
 End Namespace
--- Views/Home/Test Page.aspx
+++ Views/Home/Test Page.aspx
@@ -1,8 +1,36 @@
-﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Views/Shared/BlogSite.Master" CodeBehind="Test Page.aspx.vb" Inherits="VB.net_BlogPosts.Test_Page, VB.net_BlogPosts.DatabaseHandler"%>
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Views/Shared/BlogSite.Master" CodeBehind="Test Page.aspx.vb" Inherits="VB.net_BlogPosts.Test_Page"%>
 
 
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolderBlog" runat="server">
+
+    <%@ Import Namespace="DatabaseHandling"%> 
+
+
+    <% Dim blogList As List(Of VB.net_BlogPosts.DatabaseHandling.BlogPost) = New List(Of VB.net_BlogPosts.DatabaseHandling.BlogPost)
+
+
+        Dim blog1 = New VB.net_BlogPosts.DatabaseHandling.BlogPost()
+        blog1.Id = 1
+        blog1.Title = "Title1"
+        blog1.Content = "Content 1"
+
+        Dim blog2 = New VB.net_BlogPosts.DatabaseHandling.BlogPost()
+        blog2.Id = 2
+        blog2.Title = "Title2"
+        blog2.Content = "Content 2"
+        blogList.Add(blog2)
+        blogList.Add(blog1)
+
+
+        Dim DbHandler As VB.net_BlogPosts.DatabaseHandling.DatabaseHandler = New VB.net_BlogPosts.DatabaseHandling.DatabaseHandler
+
+        'blogList = DbHandler.GetBlogPosts
+        DbHandler.TestConnection()
+
+
+        %>
+
 
     <% Dim posts As New List(Of Object)() %> 
 
@@ -14,18 +42,28 @@
 </div>
     <div style = "text-align: center ">
     <label >Title</label>
-    <input/>
+    <input placeholder ="Title" />
     <label>Content</label>
-    <input />
-    <button>Ok</button>
+    <input placeholder ="Content"   />
+    <button id="OkButton" onclick ="Add_Post" >Ok</button>
         </div>
 
 
-    <%For Each  As String In posts%>  
+    <%For Each blog As VB.net_BlogPosts.DatabaseHandling.BlogPost In blogList%>  
     <div>
-        <h1> <%= name%> </h1>
+        <h1 style = "text-align: center "> <%= blog.Title%> </h1>
         </div>
-        
-   <% next %>
-    
+    <div> 
+        <p style = "text-align: center " > 
+            <%= blog.Content%>
+        </p>
+    </div>
+    <div> <hr class="solid"> </div>
+    <a>Edit</a>
+    <a>delete</a>
+   <%       Next %>
+  
+
+
+
 </asp:Content>
